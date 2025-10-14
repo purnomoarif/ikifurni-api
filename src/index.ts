7fbef02f96a2c65749c23618b99ae4cdcf84ebb2
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-10-14 20:29:12 +0700

    chore: add a blank line for improved code readability in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { db } from "./lib.db/db";
 
 const app = new Hono();
+
+app.use(cors());
 
 app.get("/", (c) => {
   return c.json({
