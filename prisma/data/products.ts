cccdf7a4dda92451f822c9d9e24e4f9bcc5ffd0e
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-10-15 15:19:45 +0700

    feat:  seed products

---
+++ prisma/data/products.ts
@@ -0,0 +1,52 @@
+export const dataProducts = [
+  {
+    slug: "svenarum",
+    name: "Svenarum Meja tv dengan pintu geser, bambu",
+    imageUrl:
+      "https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/597/0959740_PE806180_S5.webp",
+    price: 1899000,
+    stock: 20,
+    description:
+      "Bangku TV kokoh dengan banyak ruang untuk konsol game dan gadget media di balik pintu antidebu. Pilihan sempurna jika Anda menginginkan solusi penyimpanan luas yang menyebarkan nuansa musim panas sepanjang tahun.",
+  },
+  {
+    slug: "havsta",
+    name: "Havsta Lemari 3 laci, hitam, 81x47x89 cm",
+    imageUrl:
+      "https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/268/1426884_PE980049_S4.webp",
+    price: 3999000,
+    stock: 15,
+    description:
+      "Lemari laci yang sederhana namun fungsional yang cocok dengan segala dekor, di kamar tidur atau di mana saja. Ada ruang untuk pakaian favorit Anda dan ranjang ekstra.",
+  },
+  {
+    slug: "landskrona",
+    name: "landskrona Sofa 3 dudukan, grann/bomstad emas-cokelat/logam",
+    imageUrl:
+      "https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/021/0602109_PE680178_S5.webp",
+    price: 13995000,
+    stock: 5,
+    description:
+      "Kemewahan menjadi panduan kami. Kami menggunakan isian busa yang elastis untuk kenyamanan, kulit grain tebal di area kontak - karena terlihat luar biasa, dan bahan berlapis yang tahan lama menjaga harga tetap terjangkau.",
+  },
+  {
+    slug: "styrspel",
+    name: "Styrspel Kursi gaming, abu-abu tua/abu-abu",
+    imageUrl:
+      "https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/153/1115362_PE872046_S5.webp",
+    price: 4599000,
+    stock: 10,
+    description:
+      "Kursi gaming STYRSPEL membantu Anda bermain dengan maksimal. Sesuaikan sandaran kepala, penyangga pinggang, sandaran tangan, serta ketinggian dan kedalaman kursi - bahan jala membuat Anda tetap sejuk saat permainan memanas!",
+  },
+  {
+    slug: "latt",
+    name: "Latt Meja anak dengan 2 kursi, putih/kayu pinus",
+    imageUrl:
+      "https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/763/0876334_PE613575_S4.webp",
+    price: 399000,
+    stock: 25,
+    description:
+      "Dengan meja dan kursi ini Anda dapat dengan mudah membuat sudut seni dan kerajinan yang tahan lama untuk anak. Bingkai terbuat dari kayu pinus padat dan memiliki permukaan foil putih di atasnya yang mudah dibersihkan.",
+  },
+];
