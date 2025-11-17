889e0ea60100cf5b5ec109075a0d37cd99c382ca
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-11-17 13:49:02 +0700

    feat: put cart items

--- src/modules/cart/route.ts
+++ src/modules/cart/route.ts
@@ -1,7 +1,7 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 
 import { checkAuthorized } from "../auth/middleware";
-import { CartSchema } from "./schema";
+import { CartItemSchema, CartSchema } from "./schema";
 import { db } from "../../lib/db";
 
 export const cartRoute = new OpenAPIHono();
@@ -36,5 +36,43 @@ cartRoute.openapi(
     }
 
     return c.json(cart);
+  }
+);
+
+// PUT /cart/items
+cartRoute.openapi(
+  createRoute({
+    method: "put",
+    path: "/items",
+    middleware: checkAuthorized,
+    responses: {
+      200: {
+        description: "Add item to user's cart",
+        content: { "application/json": { schema: CartItemSchema } },
+      },
+      404: { description: "User cart not found" },
+    },
+  }),
+  async (c) => {
+    const user = c.get("user");
+
+    const cart = await db.cart.findUnique({
+      where: { userId: user.id },
+      include: { items: { include: { product: true } } },
+    });
+
+    if (!cart) {
+      return c.notFound();
+    }
+
+    const newCartItem = await db.cartItem.create({
+      data: {
+        cartId: cart.id,
+        productId: "",
+        quantity: 1,
+      },
+    });
+
+    return c.json(newCartItem);
   }
 );
